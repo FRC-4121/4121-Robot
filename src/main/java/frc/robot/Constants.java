66bc58ed925e39c76ee808205a9229f4c24d6b62
Matthew Cornell <mcornell400708@gmail.com>
2025-02-18 18:54:08 -0500

    Finished adding auto paths

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,7 +39,7 @@ public final class Constants {
     public static final double autoLeftGyroCorrection = 53;
     public static final double rightGyroCorrection = 307;// Gyro correction in degrees (was 307)
     public static final double autoRightGyroCorrection = -53;
-    public static final double kGearRatio = 9.25; //7.15 for new chassis
+    public static final double kGearRatio = 9.3; //7.15 for new chassis
 
     // Swerve drive PID constants
     public static final double kAnglePIDkp = 0.0145;
