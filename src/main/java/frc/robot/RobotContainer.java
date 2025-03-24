4e95f32259c0eae03a1c74c64e01d2d7d76e4800
ChickenKing-III <linkgeig@gmail.com>
2025-03-24 19:17:47 -0400

    Strip everything but the swerve drive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,15 +25,11 @@ public class RobotContainer {
   // Declare Driver Controllers
   private final XboxController xbox;
   private final XboxController secondaryXbox;
-  private final Joystick launchpad;
 
   // ===Subsystems===//
 
   // Declare Subsystems
   private final SwerveDriveWPI swerve;
-  private final ElevatorMM elevator;
-  private final CClaw claw;
-  private final Climber climber;
 
   // ===Extra Systems===//
 
@@ -50,11 +46,6 @@ public class RobotContainer {
   private final DriveWithJoysticks fieldDriveCommand;
   private final ChangeSpeedCommand changeSpeedCommand;
   private final ChangeDriveMode changeModeCommand;
-  private final ResetRobot resetRobot;
-
-  // Declare mechanism commands
-  private final MoveElevator moveElevatorCommand;
-  private final RotateCClaw rotateClawCommand;
 
   // Declare KillAuto Commands
   private final KillAutoCommand killAuto;
@@ -64,27 +55,9 @@ public class RobotContainer {
   // Declare Xbox Buttons and Triggers
   private final Trigger changeSpeedButton;
   private final Trigger changeModeButton;
-  private final Trigger clawHomeButton;
-  private final Trigger climbButton;
-  private final Trigger elevatorHomeButton;
-  private final Trigger elevatorCoral1Button;
-  private final Trigger elevatorCoral2Button;
-  private final Trigger elevatorCoral3Button;
-  private final Trigger elevatorCoral4Button;
-  private final Trigger coralScoreButton;
-  private final Trigger coralIntakeButton;
-  private final Trigger algaeIntakeButton;
   private final Trigger alignLeftButton;
   private final Trigger alignRightButton;
-
-  // Declare Launchpad (OI) Buttons/Switches
   private final Trigger killAutoButton;
-  private final Trigger resetEncodersButton;
-  private final Trigger blueTeamButton;
-  private final Trigger redTeamButton;
-  private final Trigger parkButton;
-  private final Trigger resetRobotButton;
-  private final Trigger safetyOverrideButton;
 
   // ===PathPlanner=== //
 
@@ -103,13 +76,9 @@ public class RobotContainer {
     // Initialize driver controllers
     xbox = new XboxController(1);
     secondaryXbox = new XboxController(0);
-    launchpad = new Joystick(2);
 
     // Initialize Subsystems
     swerve = new SwerveDriveWPI();
-    elevator = new ElevatorMM();
-    claw = new CClaw();
-    climber = new Climber();
 
     // Initialize extra systems
     table = new NetworkTableQuerier();
@@ -122,27 +91,12 @@ public class RobotContainer {
     fieldDriveCommand = new DriveWithJoysticks(swerve, xbox, table);
     changeSpeedCommand = new ChangeSpeedCommand();
     changeModeCommand = new ChangeDriveMode();
-    resetRobot = new ResetRobot(claw, elevator, swerve, climber);
-
-    // Initialize mechanism commands
-    moveElevatorCommand = new MoveElevator(elevator, secondaryXbox);
-    rotateClawCommand = new RotateCClaw(claw, secondaryXbox);
 
     // Initialize KillAuto Commands
     killAuto = new KillAutoCommand();
 
     // Register named commands for PathPlanner
     // registerPathPlannerCommands();
-    NamedCommands.registerCommand("Elevator L4", CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Coral4, CClaw.ClawPositions.L4Score));
-    NamedCommands.registerCommand("Elevator L3", CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Coral3, CClaw.ClawPositions.Home));
-    NamedCommands.registerCommand("Elevator L2", CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Coral2, CClaw.ClawPositions.Home));
-    NamedCommands.registerCommand("Elevator L1", CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Coral1, CClaw.ClawPositions.L1Score));
-    NamedCommands.registerCommand("Claw L4", claw.autoRotate(CClaw.ClawPositions.L4Score));
-    NamedCommands.registerCommand("Claw L1", claw.autoRotate(CClaw.ClawPositions.L1Score));
-    NamedCommands.registerCommand("Shoot Coral", claw.scoreCoral());
-    NamedCommands.registerCommand("Home Elevator", CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Load, CClaw.ClawPositions.Home));
-    NamedCommands.registerCommand("Intake Coral", CombinedCommands.combinedLoad(claw, elevator)
-    .withDeadline(Commands.waitSeconds(0.5)));
     NamedCommands.registerCommand("Stop Drive", swerve.stopDriving());
 
     // Create an auto command chooser
@@ -152,51 +106,17 @@ public class RobotContainer {
     // Initialize Xbox Buttons
     changeSpeedButton = new JoystickButton(xbox, xboxYButton);
     changeModeButton = new JoystickButton(xbox, xboxXButton);
-    clawHomeButton = new JoystickButton(xbox, xboxAButton);
-    climbButton = new JoystickButton(xbox, xboxBButton);
-    // climbButton = new Trigger(() -> xbox.getRightTriggerAxis() >
-    // triggerThreshold);
-    elevatorCoral1Button = new JoystickButton(secondaryXbox, xboxAButton);
-    elevatorCoral2Button = new JoystickButton(secondaryXbox, xboxBButton);
-    elevatorCoral3Button = new JoystickButton(secondaryXbox, xboxXButton);
-    elevatorCoral4Button = new JoystickButton(secondaryXbox, xboxYButton);
-    coralScoreButton = new JoystickButton(secondaryXbox, xboxRightBumper);
-    coralIntakeButton = new JoystickButton(secondaryXbox, xboxLeftBumper);
-    algaeIntakeButton = new Trigger(() -> secondaryXbox.getLeftTriggerAxis() > triggerThreshold);
-    elevatorHomeButton = new Trigger(() -> secondaryXbox.getRightTriggerAxis() > triggerThreshold);
+    killAutoButton = new JoystickButton(xbox, xboxAButton);
 
     // POVs only return the polar angle; thankfully we don't care too much about it
     alignLeftButton = new JoystickButton(xbox, xboxLeftBumper);
     alignRightButton = new JoystickButton(xbox, xboxRightBumper);
-
-    // Initialize Launchpad (OI) Buttons/Switches
-    killAutoButton = new JoystickButton(launchpad, LaunchPadButton1);
-    parkButton = new JoystickButton(launchpad, LaunchPadButton3);
-    blueTeamButton = new JoystickButton(launchpad, LaunchPadSwitch5top);
-    redTeamButton = new JoystickButton(launchpad, LaunchPadSwitch5bottom);
-    resetRobotButton = new JoystickButton(launchpad, LaunchPadSwitch1top);
-    resetEncodersButton = new JoystickButton(launchpad, LaunchPadSwitch2top);
-    safetyOverrideButton = new JoystickButton(launchpad, LaunchPadSwitch4);
 
     // Configure the button bindings
     configureButtonBindings();
 
     // Configure default subsystem commands
     configureDefaultCommands();
-
-    climber.setDefaultCommand(Commands.run(() -> {
-      double trig = xbox.getLeftTriggerAxis();
-      if (trig > 0.1) {
-        climber.runClimber(trig * 0.5);
-        return;
-      }
-      trig = xbox.getRightTriggerAxis();
-      if (trig > 0.1) {
-        climber.runClimber(-trig * 0.5);
-        return;
-      }
-      climber.runClimber(0);
-    }, climber));
 
     bestTags.filter = tagFilter;
   }
@@ -213,40 +133,6 @@ public class RobotContainer {
     // Teleop Commands
     changeSpeedButton.onTrue(changeSpeedCommand);
     changeModeButton.onTrue(changeModeCommand);
-    clawHomeButton.onTrue(claw.returnHome());
-    elevatorHomeButton.onTrue(CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Load, CClaw.ClawPositions.Home));
-    elevatorCoral1Button.onTrue(
-        Commands.either(
-            CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Coral1, CClaw.ClawPositions.L1Score),
-            CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Algae1, CClaw.ClawPositions.Algae1),
-            () -> claw.hasCoral()));
-    elevatorCoral2Button.onTrue(CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Coral2, CClaw.ClawPositions.Home));
-    elevatorCoral3Button.onTrue(CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Coral3, CClaw.ClawPositions.Home));
-    elevatorCoral4Button.onTrue(
-        Commands.either(
-          CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Coral4, CClaw.ClawPositions.L4Score),
-            CombinedCommands.moveClaw(claw, elevator, ElevatorMM.ElevatorPositions.Algae2, CClaw.ClawPositions.Algae2),
-            () -> claw.hasCoral()));
-    coralIntakeButton.onTrue(CombinedCommands.combinedLoad(claw, elevator)
-        .withDeadline(Commands.waitSeconds(1).andThen(Commands.idle().until(coralIntakeButton))));
-    coralScoreButton.onTrue(
-        Commands.either(
-            CombinedCommands.shootCoral(claw, elevator),
-            claw.algaeDeposit(),
-            () -> claw.hasCoral()));
-    algaeIntakeButton.onTrue(claw.algaeIntake());
-    climbButton.onTrue(climber.new Climb());
-    resetRobotButton.onTrue(resetRobot);
-    resetEncodersButton.whileTrue(Commands.runOnce(() -> {
-      claw.killMotor();
-      climber.killMotor();
-      elevator.killMotor();
-      claw.zeroIntake();
-      climber.zeroEncoder();
-      elevator.zeroPosition();
-    }));
-    safetyOverrideButton.onTrue(Commands.runOnce(() -> claw.setSafety(false)));
-    safetyOverrideButton.onFalse(Commands.runOnce(() -> claw.setSafety(true)));
     alignLeftButton.whileTrue(CombinedCommands.autoAlignBest(swerve, new AutoAlignBase.Alignment() {
       {
         distance = 0.14;
@@ -269,8 +155,6 @@ public class RobotContainer {
   private void configureDefaultCommands() {
 
     swerve.setDefaultCommand(fieldDriveCommand);
-    elevator.setDefaultCommand(moveElevatorCommand);
-    claw.setDefaultCommand(rotateClawCommand);
 
   }
 
@@ -296,29 +180,21 @@ public class RobotContainer {
    * switch position
    */
   public void getAllianceColor() {
-
-    if (redTeamButton.getAsBoolean())
-      Mutables.blueAlliance = false;
-    else if (blueTeamButton.getAsBoolean())
-      Mutables.blueAlliance = true;
-    else {
-      // TODO: warn someone
-      Mutables.blueAlliance = true;
-    }
+    Mutables.blueAlliance = true;
   }
 
   /**
    * Park and unpark the robot
    */
   public void getParkSelection() {
-    if (parkButton.getAsBoolean() == true) {
-      Mutables.isParked = true;
-      SmartDashboard.putBoolean("Robot Parked", true);
-      // parkCommand.execute();
-    } else {
-      Mutables.isParked = false;
-      SmartDashboard.putBoolean("Robot Parked", false);
-    }
+    // if (parkButton.getAsBoolean() == true) {
+    //   Mutables.isParked = true;
+    //   SmartDashboard.putBoolean("Robot Parked", true);
+    //   // parkCommand.execute();
+    // } else {
+    //   Mutables.isParked = false;
+    //   SmartDashboard.putBoolean("Robot Parked", false);
+    // }
   }
 
   /**
@@ -329,8 +205,6 @@ public class RobotContainer {
   public void zeroRobot() {
     swerve.zeroGyro();
     swerve.zeroEncoders();
-    elevator.zeroPosition();
-    claw.zeroIntake();
   }
 
   /**
@@ -379,8 +253,7 @@ public class RobotContainer {
    * 
    */
   public void clearMotorRequests() {
-    claw.killMotor();
-    elevator.killMotor();
+    
   }
 
   /**
@@ -389,8 +262,7 @@ public class RobotContainer {
    * 
    */
   public void setClawSafety() {
-    claw.setSafety(!safetyOverrideButton.getAsBoolean());
-    claw.setRotation(CClaw.ClawPositions.Home);
+    
   }
 
 }
--- src/main/java/frc/robot/commands/CombinedCommands.java
+++ src/main/java/frc/robot/commands/CombinedCommands.java
@@ -5,8 +5,6 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Constants.Mutables;
 import frc.robot.ExtraClasses.NetworkTableQuerier;
 import frc.robot.commands.AutoAlignBase.ToMove;
-import frc.robot.subsystems.CClaw;
-import frc.robot.subsystems.ElevatorMM;
 import frc.robot.subsystems.SwerveDriveWPI;
 
 /**
@@ -18,30 +16,6 @@ public class CombinedCommands {
 
   }
 
-  public static Command combinedLoad(CClaw claw, ElevatorMM elevator) {
-    return claw.new RotateClawAndWait(CClaw.ClawPositions.Home)
-        .andThen(Commands.idle(claw, elevator).until(() -> Mutables.isClawClear))
-        .andThen(elevator.new PositionElevatorAndWait(ElevatorMM.ElevatorPositions.Load)) // start by moving to the
-                                                                                          // level
-        .andThen(
-            claw.autoRotate(CClaw.ClawPositions.Load).andThen(claw.intakeCoral()) // move the the load position and
-                                                                                  // intake coral
-                .deadlineFor(claw.new WithoutSafety().alongWith(Commands.idle(elevator)))); // while that's going, we
-                                                                                            // want to disable safety
-  }
-
-  public static Command moveClaw(CClaw claw, ElevatorMM elevator, double elevatorPos, double clawPos) {
-    return claw.new RotateClawAndWait(CClaw.ClawPositions.Home - 0.15)
-        .andThen(elevator.new PositionElevatorAndWait(elevatorPos))
-        .andThen(
-            claw.new RotateClawAndWait(clawPos))
-        .withDeadline(Commands.waitSeconds(2.0));
-  }
-
-  public static Command shootCoral(CClaw claw, ElevatorMM elevator) {
-    return claw.scoreCoral().andThen(claw.new RotateClawAndWait(CClaw.ClawPositions.Home))
-        .andThen(elevator.positionElevator(ElevatorMM.ElevatorPositions.Load));
-  }
 
   public static Command autoAlignBest(SwerveDriveWPI swerve, AutoAlignBase.Alignment align,
       NetworkTableQuerier.BestTag best, long[] filter) {
