a62cc9bdfef51de6c94213af75d17842366155b3
Tim Fuller <tim.fuller65@gmail.com>
2026-01-10 11:53:46 -0500

    Import into 2026

--- src/main/java/frc/robot/subsystems/ElevatorMM.java
+++ src/main/java/frc/robot/subsystems/ElevatorMM.java
@@ -65,10 +65,10 @@ public class ElevatorMM extends SubsystemBase {
     public static final double Load = 0;
     public static final double Coral1 = 1;
     public static final double Coral2 = 16;
-    public static final double Coral3 = 57;
-    public static final double Coral4 = 119;
-    public static final double Algae1 = 31;
-    public static final double Algae2 = 80;
+    public static final double Coral3 = 56;
+    public static final double Coral4 = 126;
+    public static final double Algae1 = 33;
+    public static final double Algae2 = 70.7;
     public static final double Processor = 10;
     public static final double Barge = 10;
   }
